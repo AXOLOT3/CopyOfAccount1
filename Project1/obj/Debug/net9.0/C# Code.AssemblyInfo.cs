224835c7e1e85da40167ae64156cc9049631538d
AXOLOT3 <jr1108556@student.francistuttle.edu>
2026-02-18 11:27:10 -0600

    Random stuff

--- Project1/obj/Debug/net9.0/C# Code.AssemblyInfo.cs
+++ Project1/obj/Debug/net9.0/C# Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8ea67a756983ba538f5825a0608f5e3729daa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
