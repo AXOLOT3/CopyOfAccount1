37d1bbf3e7ab53837fbeeccd621b5bde79c4f3bd
AXOLOT3 <jr1108556@student.francistuttle.edu>
2026-02-20 09:38:40 -0600

    starting part b

--- Project1/obj/Debug/net9.0/C# Code.AssemblyInfo.cs
+++ Project1/obj/Debug/net9.0/C# Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26f9cca270ecc92653b0a9d056f47c42b662094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acca5cd062320f238c1e2cb142dca3040ed5b765")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
